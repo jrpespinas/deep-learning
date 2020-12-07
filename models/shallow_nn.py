ba0b1bed18f73243141d4de48397b295375b91be
jrpespinas <jrpespinas@gmail.com>
2020-12-07 15:58:43 +0800

    feat: add function for initializing parameters

--- models/shallow_nn.py
+++ models/shallow_nn.py
@@ -59,6 +59,24 @@ def layer_size(X, Y, hidden_layer: int = 5):
     return input_layer, hidden_layer, output_layer
 
 
+def initialize_parameters(input_layer, hidden_layer, output_layer, seed: int = 69, constant: float = 0.01):
+    np.random.seed(seed)
+
+    weights_1 = np.random.randn(input_layer, hidden_layer) * constant
+    bias_1 = np.zeros((hidden_layer, 1))
+    weights_2 = np.random.randn(output_layer, hidden_layer) * constant
+    bias_2 = np.zeros((output_layer, 1))
+
+    parameters = {
+        "weights_1": weights_1,
+        "bias_1": bias_1,
+        "weights_2": weights_2,
+        "bias_2": bias_2
+    }
+
+    return parameters
+
+
 def main():
     pass
 
