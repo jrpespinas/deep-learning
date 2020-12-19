0d90cdc0316b015492177a0cdaee0889aaad9191
jrpespinas <jrpespinas@gmail.com>
2020-12-19 17:41:52 +0800

    refractor: change activations into private functions

--- models/neural_networks.py
+++ models/neural_networks.py
@@ -2,32 +2,29 @@
 
 import numpy as np
 
+
 class NeuralNetwork:
     def __init__(self):
         pass
-    
-    def initialize_parameters(self):
-        pass
 
-    def update_parameters(self):
-        pass
-
-    def relu(self, z):
+    def __relu(self, z):
         return np.maximum(0, z)
 
-    def sigmoid(self, z):
+    def __sigmoid(self, z):
         return 1 / np.add(1, np.exp(-z))
 
-    def relu_prime(self, z):
+    def __relu_prime(self, z):
         z[z <= 0] = 0
         z[z > 0] = 1
         return z
-    
-    def sigmoid_prime(self, z):
+
+    def __sigmoid_prime(self, z):
         return self.sigmoid(z) * (1 - self.sigmoid(z))
+
 
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
