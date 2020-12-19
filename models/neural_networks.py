c9b0a3ecf0c131648814440145c3c8465e0364b3
jrpespinas <jrpespinas@gmail.com>
2020-12-19 21:43:42 +0800

    refractor: rename method and convert to static method

--- models/neural_networks.py
+++ models/neural_networks.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class NeuralNetwork:
     def __init__(self):
-        self.architecture = {}
+        self.architecture = []
 
     def relu(self, z):
         return np.maximum(0, z)
@@ -21,14 +21,17 @@ class NeuralNetwork:
     def sigmoid_prime(self, z):
         return self.sigmoid(z) * (1 - self.sigmoid(z))
 
-    def linear(self, output_dims, input_dims, constant: float = 0.01):
+    @staticmethod
+    def layer(output_dims, input_dims, constant: float = 0.01):
         weights = np.random.randn(output_dims, input_dims) * constant
         bias = np.zeros((output_dims, 1))
-        return weights, bias
+
+        return [weights, bias]
 
 
 def main():
-    pass
+    model = NeuralNetwork()
+    model.add(NeuralNetwork.layer(15, 15))
 
 
 if __name__ == "__main__":
